774f86e01291971af26265f9239a2aa3b3a60fc6
amine sakhri <sakhriaminee2003@gmail.com>
2026-04-30 10:39:33 +0200

    faire la connexion entre mysql et php

---
+++ student/config.php
@@ -0,0 +1,7 @@
+<?php
+try {
+    $conn = new PDO("mysql:host=localhost;dbname=edusync;charset=utf8", "root", "");
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (Exception $e) {
+    die("Erreur DB : " . $e->getMessage());
+}
