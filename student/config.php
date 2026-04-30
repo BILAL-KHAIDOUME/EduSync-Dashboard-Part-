e1bc1f3fb796f157bd35c4053934f127baa2e188
amine sakhri <sakhriaminee2003@gmail.com>
2026-04-30 14:49:43 +0200

    la session en logout.php

--- student/config.php
+++ student/config.php
@@ -1,7 +1,12 @@
 <?php
+
+
 try {
     $conn = new PDO("mysql:host=localhost;dbname=edusync;charset=utf8", "root", "");
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (Exception $e) {
     die("Erreur DB : " . $e->getMessage());
 }
+
+// ⚠️ Simulation utilisateur connecté (remplacer par login)
+$_SESSION['user_id'] = 3; 
--- student/date.sql
+++ student/date.sql
@@ -9,7 +9,7 @@ CREATE TABLE roles (
 CREATE TABLE classes (
     id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(50) NOT NULL,
-    classroom_number INT NOT NULL
+    classroom_number INT 
 );
 
 CREATE TABLE users (
@@ -23,6 +23,8 @@ CREATE TABLE users (
     FOREIGN KEY (role_id) REFERENCES roles(id),
     FOREIGN KEY (class_id) REFERENCES classes(id)
 );
+
+
 
 CREATE TABLE courses (
     id INT AUTO_INCREMENT PRIMARY KEY,
@@ -55,6 +57,44 @@ CREATE TABLE enrollments (
 );
 
 
+-- data :
+
+INSERT INTO roles (label) VALUES
+('admin'),
+('teacher'),
+('student');
+
+
+INSERT INTO classes(name,classroom_number) VALUES
+('DEV101', 1),
+('DEV102', 2),
+('DEV103', 3);
+
+
+INSERT INTO users (firstname, lastname, email, password, role_id) VALUES
+('Ali', 'Admin', 'admin@edusync.com', '123', 1),
+('Sara', 'Teacher', 'teacher@edusync.com', '123', 2),
+('Amine', 'Student', 'amine@edusync.com', '123', 3),
+('Yassine', 'Student', 'yassine@edusync.com', '123', 3),
+('Omar', 'Student', 'omar@edusync.com', '123', 3);
+
+
+INSERT INTO students (date_of_birth, student_number, class_id, user_id) VALUES
+('2001-05-10', 1001, 1, 3),
+('2002-03-22', 1002, 1, 4),
+('2000-12-01', 1003, 2, 5);
 
 
 
+INSERT INTO courses (title, description, total_hours, professor_id) VALUES
+('PHP', 'Backend development with PHP', 40, 2),
+('SQL', 'Database design and queries', 30, 2),
+('HTML/CSS', 'Frontend basics', 25, 2);
+
+
+
+INSERT INTO enrollments (enrolled_at, status, student_id, course_id) VALUES
+(CURDATE(), 'Actif', 1, 1),
+(CURDATE(), 'Actif', 1, 2),
+(CURDATE(), 'Actif', 2, 1),
+(CURDATE(), 'Terminé', 3, 3);
